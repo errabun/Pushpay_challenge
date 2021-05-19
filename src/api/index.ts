ebda7c4d5baba6174401bd9c2160b6398560cde9
Alex Beckwith <alfonz129@gmail.com>
2021-05-19 16:13:53 -0600

    add starter app

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+export async function fetchJson(url: string, init?: RequestInit) {
+  const response = await fetch(
+    `https://swapi.dev/api/${url}/`,
+    {
+      ...init ?? {},
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+
+
+  return response.json()
+}
---
+++ src/components/People/People.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { fetchJson } from '../../api'
+import { PersonType } from '../../types'
+import Person from '../Person'
+
+function People() {
+  const [people, setPeople] = React.useState<PersonType[]>([])
+
+  React.useEffect(() => {
+    fetchJson('people').then(peopleResponse => setPeople(peopleResponse.results))
+  }, [])
+
+  return (
+    <div>
+      {people.map(person => <Person person={person} />)}
+    </div>
+  )
+}
+
+export default People
